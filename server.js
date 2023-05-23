de8ebdd3fd1c9c36b9d730a92dd21ac78d73dbee
seanarch <zhuqingyun@outlook.com>
2023-05-23 15:43:26 -0600

    Update query date

--- server.js
+++ server.js
@@ -33,8 +33,34 @@ const schema = buildSchema(`
 `);
 
 const root = {
-  description: "Red Shoe",
-  price: 42.12,
+  products: [
+    {
+      id: "redshoe",
+      description: "Red Shoe",
+      price: 42.12,
+    },
+    {
+      id: "bluejean",
+      description: "Blue Jeans",
+      price: 55.55,
+    },
+  ],
+  orders: [
+    {
+      date: "2022-05-05",
+      subtotal: 90.22,
+      items: [
+        {
+          product: {
+            id: "redshoe",
+            description: "Old Red Shoe",
+            price: 45.11,
+          },
+          quantity: 2,
+        },
+      ],
+    },
+  ],
 };
 
 const app = express();
