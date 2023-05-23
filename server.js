774cecf7c7e7b5736f8b15a77e6f0256ae7b4348
seanarch <zhuqingyun@outlook.com>
2023-05-23 16:28:48 -0600

    Update loadFilesSync extensions

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const path = require("path");
 const { loadFilesSync } = require("@graphql-tools/load-files");
 const { graphqlHTTP } = require("express-graphql");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 
-const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+const typesArray = loadFilesSync("**/*", {
+  extensions: ["graphql"],
+});
 
 const schema = makeExecutableSchema({
   typeDefs: typesArray,
